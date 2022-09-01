e49fd98d9d90fe7b77b46e55dfdad8d3105c1814
God.Su <93633273+955xiaoSu@users.noreply.github.com>
2022-09-01 16:28:47 +0800

    Update 蛇形矩阵.cpp

--- 蛇形矩阵.cpp
+++ 蛇形矩阵.cpp
@@ -1,6 +1,6 @@
-// ͨ�����������Եõ����ķ�Χ
-// ���������˼�룬��������������ĸ���֮��Ϊ��ֵ 
-// ÿ����֮���й��ɣ����ĸ�����ʼ���н���
+// 通过层数，可以得到数的范围
+// 层序遍历的思想，层数与输出的数的个数之和为定值 
+// 每个数之间有规律，从哪个数开始加有讲究
 
 #include <iostream>
 #include <vector>
@@ -9,9 +9,7 @@ using namespace std;
 int main() {
 	int piles = 0;  cin >> piles;
 	vector<int> proIncrease;
-	
 	for (int digit = 0; digit <= piles; digit++)  proIncrease.push_back(digit);
-	
 	int basicNum = 1, temp = 0;
 	for (int row = 1; row <= piles; row++) {
 		basicNum += proIncrease[row - 1];
@@ -23,7 +21,6 @@ int main() {
 		}
 		cout << endl;
 	}
-	
 	return 0;
 } 
 
