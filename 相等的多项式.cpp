b0be47ecfe77827892a61e1e3efd44e65688389e
God.Su <93633273+955xiaoSu@users.noreply.github.com>
2022-09-01 16:45:11 +0800

    Update 相等的多项式.cpp

--- 相等的多项式.cpp
+++ 相等的多项式.cpp
@@ -1,5 +1,3 @@
-// 回溯法构建排列数，模拟真实的多项式相乘 
-
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -8,13 +6,8 @@ int origin[22], realAns[22], givenAns[22];
 int test = 0;
 
 void sour(int nowFloor, int digits, int sum) {
-	cout << "nowFloor : " << nowFloor << " digits : " << digits << " sum : " << sum << endl;
-	cout << givenAns[0] << " " << givenAns[1] << " " << givenAns[2] << " " << endl;
 	if (nowFloor == test) {
-		//cout << givenAns[0] << " " << givenAns[1] << " " << givenAns[2] << " " << endl;
 		realAns[digits] += sum;
-		//cout << "nowFloor : " << nowFloor << " digits : " << digits << " sum : " << sum << endl;
-		//cout << givenAns[0] << " " << givenAns[1] << " " << givenAns[2] << " " << endl;
 		return;
 	}
 	sour(nowFloor + 1, digits, sum);
@@ -24,12 +17,8 @@ void sour(int nowFloor, int digits, int sum) {
 int main() {
 	while (cin >> test && test) {
 		memset(realAns, 0, sizeof(realAns));
-		
-		for (int index = 0; index < test; index++)	cin >> origin[index];
-		for (int index = 0; index < test; index++) cin >> givenAns[index];
-		
-		cout << "Strat: ";
-		cout << givenAns[0] << " " << givenAns[1] << " " << givenAns[2] << " " << endl;
+		for (int index = 0; index < test; index++)  cin >> origin[index];
+		for (int index = 0; index < test; index++)  cin >> givenAns[index];
 		sour(0, 0, 1);
 		bool flag = true;
 		for (int index = 0; index < test; index++) {
@@ -38,9 +27,8 @@ int main() {
 				break;
 			}
 		}
-		
-		if (flag) cout << "Y" << endl;
-		else cout << "N" << endl;
+		if (flag)  cout << "Y" << endl;
+		else  cout << "N" << endl;
 	} 
 	return 0;
 }
